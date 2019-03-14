4dfdbfbf2d6cd5573084dd6c0b6f92705dc18510
michchen <michchen58@gmail.com>
2019-03-13 22:59:29 -0700

    Begin styling

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
 import Input from './Input.jsx'
 import DeleteItemButton from './DeleteItemButton.jsx'
 import EditButton from './EditButton.jsx';
+
+const ItemElt = styled.div`
+  color:red;
+  background-color: rgb(253, 253, 253);
+  height: 55px;
+  border-color: rgb(240, 241, 244);
+  border-width: 10px;
+`;
+
 
 class ListItem extends Component {
   constructor(props) {
@@ -12,8 +23,6 @@ class ListItem extends Component {
     this.addItemInputOnChange = this.addItemInputOnChange.bind(this);
   }
 
-
-
   addItemInputOnChange(e) {
     this.setState({
       text: e.target.value
@@ -23,7 +32,7 @@ class ListItem extends Component {
   render() {
     // debugger
     return (
-      <div>
+      <ItemElt>
         {this.props.data.editing
           /* input box */
           ? <span>
@@ -42,7 +51,7 @@ class ListItem extends Component {
         }
         <span>{this.props.data.timestamp}</span>
         <DeleteItemButton clickCb={() => this.props.deleteCb(this.props.itemKey)}/>
-      </div>
+      </ItemElt>
     );
   }
 }
